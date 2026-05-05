e51eac70500eca49c1aa5b6b4ee9974361edf308
Salvo Rinzivillo <rinzivillo@isti.cnr.it>
2026-05-05 17:35:37 +0200

    [JS] updating the list of explanations

--- d3_sandbox/src/instances.js
+++ d3_sandbox/src/instances.js
@@ -1,10 +1,8 @@
-const abaloneInstances = [
-  62, 83, 1073
-];
+const abaloneInstances = [72, 1162, 1245, 560, 1004, 775, 1, 609, 958, 760, 663, 460, 279, 43, 1182, 645, 691, 1183, 326, 415];
 
-const germanInstances = [288, 294, 60, 296, 41, 47, 105, 74, 1, 223, 298, 27, 128, 181, 56, 224, 31, 80, 99, 141];
+const germanInstances = [218, 260, 200, 77, 109, 198, 272, 78, 100, 236, 59, 234, 108, 243, 252, 56, 92, 145, 298, 42];
 
-const irisInstances = [20, 34, 39];
+const irisInstances = [19, 3, 27, 30, 6, 9, 17, 20, 24, 41, 23, 13, 22, 16, 26, 36, 11, 33, 12, 25];
 
 function buildDatasetEntries(datasetLabel, basePath, ids) {
   const base = import.meta.env.BASE_URL.replace(/\/$/, '');
